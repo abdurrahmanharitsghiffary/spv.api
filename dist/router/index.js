5e3520f861b2b5bdce54e822b4e9102c4f558110
dangdang2121 <haffae124@gmail.com>
2024-08-04 14:06:32 +0700

    testing ci/cd

--- dist/router/index.js
+++ dist/router/index.js
@@ -39,6 +39,9 @@ function router(app) {
     app.use("/api/groups", group_routes_1.default);
     app.use("/api/report", report_routes_1.default);
     app.get("/api/counts", (0, validator_middlewares_1.validate)(count_schema_1.getCountsValidation), auth_middlewares_1.verifyToken, count_controller_1.getCounts);
+    app.get("hello-world", () => {
+        return "Hello world";
+    });
     app.get("/api/search", (0, validator_middlewares_1.validate)(zod_1.z.object({
         query: zod_1.z.object({
             limit: schema_1.zLimit,
